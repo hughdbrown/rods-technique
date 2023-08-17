bed8bfd4b513394dfad26ed79934f9a1f414c205
Hugh Brown <hughdbrown@yahoo.com>
2023-08-17 09:48:44 -0600

    Initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,137 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::cmp::max;
+
+use knapsack_utils::{
+    Item,
+    SearchResult,
+    // sum_weights,
+    sum_values,
+    sort_by_density,
+};
+
+fn rods_technique_helper(
+    items: &[Item],
+    i: usize,
+    limit_weight: usize,
+    weight: usize,
+    value: u64,
+    //mut max_weight: Box<Result<usize, ()>>,
+    mut max_value: u64,
+    path: &[usize],
+) -> Result<SearchResult, ()> {
+    if weight > limit_weight {
+        //println!("{weight} > {limit_weight}");
+        return Err(());
+    }
+    if i >= items.len() {
+        let r: SearchResult = (path.to_vec(), weight, value);
+        //println!("Success: {weight}/{limit_weight} {value}");
+        return Ok(r);
+    }
+
+    let mut left: Result<SearchResult, ()> = Err(());
+    let remaining_items = &items[i..];
+    let remaining_value = sum_values(remaining_items);
+    if value + remaining_value > max_value {
+        // Try with item[i]
+        // Make a new copy of the immutable path argument that has `i` appended.
+        let mut lpath: Vec<usize> = path.to_vec();
+        lpath.push(i);
+        let item = &items[i];
+        left = rods_technique_helper(
+            items,
+            i + 1,
+            limit_weight,
+            weight + item.weight,
+            value + item.value,
+            //max_weight.clone(),
+            max_value,
+            &lpath);
+
+        match left {
+            Ok(ref t) => { max_value = max(max_value, t.2); }
+            Err(_) => {},
+        }
+    }
+
+    let mut right: Result<SearchResult, ()> = Err(());
+    let remaining_items = &items[i+1..];
+    let remaining_value = sum_values(remaining_items);
+    if value + remaining_value > max_value {
+        // Try without item[i]
+        right = rods_technique_helper(
+            items,
+            i + 1,
+            limit_weight,
+            weight,
+            value,
+            //max_weight,
+            max_value,
+            path);
+    }
+
+    // Which is better?
+    match (left, right) {
+        (Ok(lvalue), Ok(rvalue)) => {
+            let best_value = if lvalue.2 > rvalue.2 { lvalue } else { rvalue };
+            return Ok(best_value);
+        },
+        (Ok(lvalue), Err(())) => return Ok(lvalue),
+        (Err(()), Ok(rvalue)) => return Ok(rvalue),
+        (Err(()), Err(())) => return Err(()),
+    }
 }
+
+pub fn rods_technique(items: &[Item], limit_weight: usize) -> Result<SearchResult, ()> {
+    let path = vec![];
+    let density_sorted_items: Vec<Item> = sort_by_density(items);
+    return rods_technique_helper(
+        &density_sorted_items,
+        0, // i = 0 (start at the first item)
+        limit_weight,
+        0, // weight = 0 (Weight of current path)
+        0, // value = 0 (Value of current path)
+        //Box::new(max_weight), // max_weight seen on completed branch
+        0, // max_value seen on completed branch
+        &path); // Current path (starts empty)
+}
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn rods_technique_1() {
+        let items = vec![
+            Item{value:1500, weight:1},
+            Item{value:2000, weight:3},
+            Item{value:3000, weight:4},
+        ];
+        match rods_technique(&items, 4) {
+            Ok(value) => assert_eq!(value.2, 3500),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn rods_technique_2() {
+        let items = vec![
+            Item{value:1500, weight:1},
+            Item{value:2000, weight:3},
+            Item{value:3000, weight:4},
+            Item{value:3300, weight:5},
+            Item{value:4000, weight:6},
+            Item{value:4200, weight:7},
+            Item{value:4400, weight:8},
+        ];
+        let expected_vals: Vec<u64> = vec![22400, 20900, 20400, 20400, 19400, 19100, 18400, 18200];
+        for (i, expected_val) in expected_vals.iter().map(|x: &u64| *x).enumerate() {
+            println!("----- Testing {i} for value {expected_val}");
+            match rods_technique(&items, 34 - i) {
+                Ok(value) => assert_eq!(value.2, expected_val),
+                Err(_) => { },
+            }
+        }
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use prng::Prng;
+use knapsack_utils::{
+    make_items,
+    sum_values,
+    sum_weights,
+    select_items,
+    Item,
+    sort_by_density,
+};
+use rods_technique::rods_technique;
+
+const NUM_ITEMS: usize = 15;
+
+const MIN_VALUE: u64 = 1;
+const MAX_VALUE: u64 = 12;
+const MIN_WEIGHT: u64 = 4;
+const MAX_WEIGHT: u64 = 15;
+const LIMIT_WEIGHT: usize = 100; 
+
+fn main() {
+    let mut prng = Prng::new();
+    let items = make_items(
+        &mut prng,
+        NUM_ITEMS,
+        MIN_VALUE, MAX_VALUE,
+        MIN_WEIGHT, MAX_WEIGHT,
+    );
+
+    println!("*** Parameters ***");
+    println!("# items:        {}", NUM_ITEMS);
+    println!("Total value:    {}", sum_values(&items));
+    println!("Total weight:   {}", sum_weights(&items));
+    println!("Allowed weight: {}", LIMIT_WEIGHT);
+    println!();
+
+    match rods_technique(&items, LIMIT_WEIGHT) {
+        Ok(result) => {
+            let (path, weight, value) = result;
+            println!("Weight = {weight}");
+            println!("Value = {value}");
+            println!("Path = {:?}", path);
+            let density_sorted: Vec<Item> = sort_by_density(&items);
+            //let selected_items: Vec<Item> = select_items(&items, &path);
+            let selected_items: Vec<Item> = select_items(&density_sorted, &path);
+            println!("Items = {:#?}", selected_items);
+            println!("Validate calculations:\n\tweight = {}\n\t value = {}", sum_weights(&selected_items), sum_values(&selected_items));
+        },
+        Err(()) => println!("No solution found!"),
+    }
+}
